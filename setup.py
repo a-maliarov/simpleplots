bfeb51cdd5c9af5eb9ca75bc9e9afa77d324c485
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2022-02-03 01:16:14 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+import os
+
+#--------------------------------------------------------------------------------------------------------------
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'simpleplots', '__version__.py'), 'r', encoding='utf-8') as f:
+    file_data = [i.replace('\n', '').replace('\'', '').split(' = ') for i in f.readlines()]
+    about = {k: v for k, v in file_data}
+
+def readme(logo_end_line=0):
+    """Extracts the logo from README file before pushing to PyPi."""
+
+    with open('README.md', 'r', encoding='utf-8') as fh:
+        long_description = ''.join(fh.readlines()[logo_end_line:])
+
+    return long_description
+
+classifiers = [
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Development Status :: 5 - Production/Stable",
+    "Natural Language :: English",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Education",
+    "Intended Audience :: Information Technology",
+]
+
+requires = [
+    "pillow ~= 9.0.0",
+]
+
+#--------------------------------------------------------------------------------------------------------------
+
+setuptools.setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    packages=['simpleplots'],
+    py_modules=['devtools', 'exceptions', 'solver', 'utils'],
+    classifiers=classifiers,
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    install_requires=requires,
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    project_urls={
+        'Source': about['__url__'],
+    },
+)
+
+#--------------------------------------------------------------------------------------------------------------
---
+++ simpleplots/__version__.py
@@ -0,0 +1,8 @@
+__title__ = 'simpleplots'
+__description__ = "simpleplots"
+__url__ = 'https://github.com/a-maliarov/simpleplots'
+__version__ = '0.0.1'
+__author__ = 'Anatolii Maliarov'
+__author_email__ = 'tly.mov@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022 Anatolii Maliarov'
