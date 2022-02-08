b86695f8a98e2a6b288ef020a720b56de2e6be3f
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2022-02-08 08:43:51 +0200

    Add "get_text_dimensions", "frange" and "scale_range" functions

---
+++ simpleplots/utils.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+"""
+simpleplots.utils
+~~~~~~~~~~~~~~~~~
+
+"""
+
+import math
+import os
+
+#-------------------------------------------------------------------------------
+
+def get_text_dimensions(text_string, font):
+    ascent, descent = font.getmetrics()
+
+    text_width = font.getmask(text_string).getbbox()[2]
+    text_height = font.getmask(text_string).getbbox()[3] + descent
+
+    return (text_width, text_height)
+
+#-------------------------------------------------------------------------------
+
+def frange(start, stop=None, step=None):
+    start = float(start)
+    if stop == None:
+        stop = start + 0.0
+        start = 0.0
+    if step == None:
+        step = 1.0
+
+    count = 0
+    while True:
+        temp = float(start + count * step)
+        if step > 0 and temp >= stop:
+            break
+        elif step < 0 and temp <= stop:
+            break
+        yield temp
+        count += 1
+
+#-------------------------------------------------------------------------------
+
+def scale_range(vmin, vmax, n=1, threshold=100):
+    dv = abs(vmax - vmin)
+    meanv = (vmax + vmin) / 2
+    if abs(meanv) / dv < threshold:
+        offset = 0
+    else:
+        offset = math.copysign(10 ** (math.log10(abs(meanv)) // 1), meanv)
+    scale = 10 ** (math.log10(dv / n) // 1)
+
+    return scale, offset
+
+#-------------------------------------------------------------------------------
