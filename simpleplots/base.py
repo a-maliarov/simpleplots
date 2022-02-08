12b662583123875045c5d3c4e8dd44ada56edd19
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2022-02-08 08:41:43 +0200

    Add "Coords", "Theme" and "Axes" dataclasses

--- simpleplots/base.py
+++ simpleplots/base.py
@@ -1,10 +1,59 @@
 # -*- coding: utf-8 -*-
 
-"""Placeholder"""
+"""
+simpleplots.base
+----------------
+
+"""
+
+from dataclasses import dataclass
+from collections import namedtuple
+from typing import Tuple
 
 #-------------------------------------------------------------------------------
 
-class Placeholder(object):
-    pass
+Coords = namedtuple("Coords", "x0 y0 x1 y1")
+
+#-------------------------------------------------------------------------------
+
+@dataclass
+class Theme:
+    figure_background_color: Tuple[int, ...]
+
+    spine_box_width_perc: float
+    spine_box_height_perc: float
+    spine_color: Tuple[int, ...]
+    spine_width: float
+
+    grid_box_width_perc: float
+    grid_box_height_perc: float
+    grid_visibility: bool
+    grid_line_color: Tuple[int, ...]
+    grid_line_width: float
+
+    tick_length_perc: float
+    tick_line_color: Tuple[int, ...]
+    tick_line_width: float
+
+    tick_label_font: str
+    tick_label_size_perc: float
+    tick_label_color: Tuple[int, ...]
+
+    point_radius: float
+
+    title_font: str
+    title_size_perc: float
+    title_color: Tuple[int, ...]
+
+#-------------------------------------------------------------------------------
+
+class Axes(object):
+
+    def __init__(self, xvalues, yvalues, color='red', linewidth=4):
+        self.xvalues = xvalues
+        self.yvalues = yvalues
+
+        self.color = color
+        self.linewidth = linewidth
 
 #-------------------------------------------------------------------------------
