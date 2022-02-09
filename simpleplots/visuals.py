50ed1a490543c012ac02e16cf62c76fbdef5a0e5
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2022-02-09 04:27:28 +0200

    Add size configuration and values mapping to PointsGrid

--- simpleplots/visuals.py
+++ simpleplots/visuals.py
@@ -63,17 +63,50 @@ class Spines(object):
 
 class PointsGrid(object):
 
-    def __init__(self, spines_width, spines_height, theme):
-        self.spines_width = spines_width
-        self.spines_height = spines_height
+    def __init__(self, spines, theme):
+        self.spines = spines
         self.theme = theme
 
-        self.width = self.spines_width * self.theme.grid_box_width_perc
-        self.height = self.spines_height * self.theme.grid_box_height_perc
-        self.horizontal_offset = (self.spines_width - self.width) / 2
-        self.vertical_offset = (self.spines_height - self.height) / 2
+        self.width = self.spines.width * self.theme.grid_box_width_perc
+        self.height = self.spines.height * self.theme.grid_box_height_perc
+        self.horizontal_offset = (self.spines.width - self.width) / 2
+        self.vertical_offset = (self.spines.height - self.height) / 2
 
-        self.x_lines_number = None
-        self.y_lines_number = None
+        self.xvalues = list()
+        self.yvalues = list()
+
+        self.x_major_ticks = list()
+        self.x_major_ticks = list()
+
+        self.cell_width = None
+        self.cell_height = None
+
+        self.x_connections = list()
+        self.y_connections = list()
+
+    def configure_size(self):
+        self.cell_width = self.width / (len(self.xvalues) - 1)
+        self.cell_height = self.height / (len(self.yvalues) - 1)
+
+    def map_values_with_coords(self):
+        self.x_connections = list()
+        for x_index, x_value in enumerate(self.xvalues):
+            x_coordinate = self.spines.horizontal_offset + self.horizontal_offset + (self.cell_width * x_index + 1)
+
+            point_data = {
+                'point_value': x_value,
+                'point_coords': x_coordinate
+            }
+            self.x_connections.append(point_data)
+
+        self.y_connections = list()
+        for y_index, y_value in enumerate(self.yvalues):
+            y_coordinate = self.spines.vertical_offset + self.vertical_offset + (self.cell_height * (len(self.yvalues) - y_index - 1))
+
+            point_data = {
+                'point_value': y_value,
+                'point_coords': y_coordinate
+            }
+            self.y_connections.append(point_data)
 
 #-------------------------------------------------------------------------------
