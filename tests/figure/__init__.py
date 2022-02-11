e8a7b2d776c888a82686028f46b1e06af5bfd332
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2022-02-11 23:09:42 +0200

    Add figure tests

---
+++ tests/figure/__init__.py
@@ -0,0 +1,1 @@
+from .test_figure import TestFigure
---
+++ tests/test_simpleplots.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from .utils import TestUtils
+from .ticker import TestTicker
+from .visuals import TestVisuals
+from .figure import TestFigure
+
+#-----------------------------------------------------------------------------
