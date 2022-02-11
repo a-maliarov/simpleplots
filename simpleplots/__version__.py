047d6deef85360b8dded35d24dad489b5ff1ef2e
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2022-02-11 11:21:55 +0200

    Update the description

--- simpleplots/__version__.py
+++ simpleplots/__version__.py
@@ -1,5 +1,5 @@
 __title__ = 'simpleplots'
-__description__ = "simpleplots"
+__description__ = "Pure Python, lightweight, Pillow-based plotting tool, focused on efficiency and prevention of memory losses."
 __url__ = 'https://github.com/a-maliarov/simpleplots'
 __version__ = '0.3.2'
 __author__ = 'Anatolii Maliarov'
