8f47502ce4e5c6a3da08113972b4a1fc6466655f
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2022-02-11 23:31:58 +0200

    Add tests for EdgeInteger and MaxNLocator

--- tests/ticker/test_ticker.py
+++ tests/ticker/test_ticker.py
@@ -1,22 +1,47 @@
 # -*- coding: utf-8 -*-
 
-from simpleplots.ticker import MaxNLocator
+from simpleplots.ticker import MaxNLocator, EdgeInteger
 import unittest
 
 #-----------------------------------------------------------------------------
 
 class TestTicker(unittest.TestCase):
 
-    def test_integers(self):
+    def test_edge_integer_negative_step(self):
+        with self.assertRaises(Exception) as context:
+            edge = EdgeInteger(-1, 0)
+
+    def test_edge_integer_closeto(self):
+        edge = EdgeInteger(1, 1)
+
+        with self.subTest():
+            self.assertTrue(edge.closeto(1, 1))
+
+        with self.subTest():
+            self.assertFalse(edge.closeto(1, 10))
+
+    def test_maxnlocator_integers(self):
         loc = MaxNLocator()
         ticks = loc.tick_values(1, 16)
         expected = [0.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0]
         self.assertListEqual(ticks, expected)
 
-    def test_floats(self):
+    def test_maxnlocator_floats(self):
         loc = MaxNLocator()
         ticks = loc.tick_values(0.1, 1.3)
         expected = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4]
+        self.assertListEqual(ticks, expected)
+
+    def test_maxnlocator_floats_reversed(self):
+        loc = MaxNLocator()
+        ticks = loc.tick_values(1.3, 0.1)
+        expected = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4]
+        self.assertListEqual(ticks, expected)
+
+    def test_maxnlocator_floats_including_only_integers(self):
+        loc = MaxNLocator(integer=True)
+        ticks = loc.tick_values(0.1, 2.5)
+        expected = [0.0, 1.0, 2.0, 3.0]
         self.assertListEqual(ticks, expected)
 
 #-----------------------------------------------------------------------------
