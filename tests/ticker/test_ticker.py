c3723c6eb8c616dfb990aec9a6a5bfc852dbd31e
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2022-02-12 07:28:31 +0200

    Update tests due to numpy implementation

--- tests/ticker/test_ticker.py
+++ tests/ticker/test_ticker.py
@@ -2,6 +2,7 @@
 
 from simpleplots.ticker import MaxNLocator, EdgeInteger
 import unittest
+import numpy as np
 
 #-----------------------------------------------------------------------------
 
@@ -23,25 +24,25 @@ class TestTicker(unittest.TestCase):
     def test_maxnlocator_integers(self):
         loc = MaxNLocator()
         ticks = loc.tick_values(1, 16)
-        expected = [0.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0]
-        self.assertListEqual(ticks, expected)
+        expected = np.asarray([0.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0])
+        self.assertListEqual(ticks.tolist(), expected.tolist())
 
     def test_maxnlocator_floats(self):
         loc = MaxNLocator()
         ticks = loc.tick_values(0.1, 1.3)
-        expected = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4]
-        self.assertListEqual(ticks, expected)
+        expected = np.asarray([0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4])
+        self.assertListEqual(ticks.tolist(), expected.tolist())
 
     def test_maxnlocator_floats_reversed(self):
         loc = MaxNLocator()
         ticks = loc.tick_values(1.3, 0.1)
-        expected = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4]
-        self.assertListEqual(ticks, expected)
+        expected = np.asarray([0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4])
+        self.assertListEqual(ticks.tolist(), expected.tolist())
 
     def test_maxnlocator_floats_including_only_integers(self):
         loc = MaxNLocator(integer=True)
         ticks = loc.tick_values(0.1, 2.5)
-        expected = [0.0, 1.0, 2.0, 3.0]
-        self.assertListEqual(ticks, expected)
+        expected = np.asarray([0.0, 1.0, 2.0, 3.0])
+        self.assertListEqual(ticks.tolist(), expected.tolist())
 
 #-----------------------------------------------------------------------------
--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from simpleplots.utils import get_text_dimensions, frange, scale_range, smartrange
+from simpleplots.utils import get_text_dimensions, scale_range, smartrange
 import unittest
+import numpy as np
 
 #-----------------------------------------------------------------------------
 
@@ -36,44 +37,28 @@ class TestUtils(unittest.TestCase):
             expected = 100
             self.assertEqual(offset, expected)
 
-    def test_frange_without_step(self):
-        to_test = list(frange(0.1, 0.2))
-        expected = [0.1, 0.2]
-        self.assertListEqual(to_test, expected)
-
-    def test_frange_with_step(self):
-        to_test = list(frange(0.1, 0.2, 0.01))
-        expected = [0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2]
-        self.assertListEqual(to_test, expected)
-
-    def test_frange_negative_numbers(self):
-        to_test = list(frange(-0.9, -0.1))
-        expected = [-0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1]
-        self.assertListEqual(to_test, expected)
-
-    def test_frange_negative_and_positive_numbers(self):
-        to_test = list(frange(-0.9, 0.2))
-        expected = [-0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2]
-        self.assertListEqual(to_test, expected)
-
     def test_smartrange_floats_without_gaps(self):
-        to_test = list(smartrange(0.1, 0.2, [0.1, 0.2]))
-        expected = [0.1, 0.2]
+        ov = np.asarray([0.1, 0.2])
+        to_test = smartrange(0.1, 0.2, ov).tolist()
+        expected = np.asarray([0.1, 0.2]).tolist()
         self.assertListEqual(to_test, expected)
 
     def test_smartrange_floats_with_gaps(self):
-        to_test = list(smartrange(0.1, 0.4, [0.1, 0.2, 0.3]))
-        expected = [0.1, 0.2, 0.3, 0.4]
+        ov = np.asarray([0.1, 0.2, 0.3])
+        to_test = smartrange(0.1, 0.4, ov).tolist()
+        expected = np.asarray([0.1, 0.2, 0.3, 0.4]).tolist()
         self.assertListEqual(to_test, expected)
 
     def test_smartrange_integers_without_gaps(self):
-        to_test = list(smartrange(1, 2, [1, 2]))
-        expected = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
+        ov = np.asarray([1, 2])
+        to_test = smartrange(1, 2, ov).tolist()
+        expected = np.asarray([1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]).tolist()
         self.assertListEqual(to_test, expected)
 
     def test_smartrange_integers_with_gaps(self):
-        to_test = list(smartrange(1, 20, list(range(1, 21))))
-        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+        ov = np.asarray(list(range(1, 21)))
+        to_test = smartrange(1, 20, ov).tolist()
+        expected = np.asarray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]).tolist()
         self.assertListEqual(to_test, expected)
 
 #-----------------------------------------------------------------------------
