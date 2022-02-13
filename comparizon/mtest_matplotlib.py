4db5d3c19b8c40907d794be98a1fb86352e57030
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2022-02-13 20:16:54 +0200

    Add comparizon results

---
+++ comparizon/mtest_matplotlib.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import shutil
+import random
+import time
+import os
+
+#-------------------------------------------------------------------------------
+
+def get_random_values(x, y):
+    xvalues = [i for i in range(x, y)]
+    yvalues = [i for i in range(x, y)]
+    random.shuffle(yvalues)
+    return xvalues, yvalues
+
+test_folder = 'mtest_mpl'
+try:
+    os.mkdir(test_folder)
+except:
+    shutil.rmtree(test_folder)
+    os.mkdir(test_folder)
+
+for i in range(100):
+    plt.figure(figsize=(16, 12))
+
+    xvalues, yvalues = get_random_values(1, 10000)
+    plt.plot(xvalues, yvalues, color='red', linewidth=5)
+
+    xvalues, yvalues = get_random_values(1, 10000)
+    plt.plot(xvalues, yvalues, color='blue', linewidth=5)
+
+    plt.savefig(f'{test_folder}/graph{i}.png')
+    plt.close()
+
+#-------------------------------------------------------------------------------
---
+++ comparizon/mtest_simpleplots.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from simpleplots import Figure
+import shutil
+import random
+import time
+import os
+
+#-------------------------------------------------------------------------------
+
+def get_random_values(x, y):
+    xvalues = [i for i in range(x, y)]
+    yvalues = [i for i in range(x, y)]
+    random.shuffle(yvalues)
+    return xvalues, yvalues
+
+test_folder = 'mtest_sp'
+try:
+    os.mkdir(test_folder)
+except:
+    shutil.rmtree(test_folder)
+    os.mkdir(test_folder)
+
+for i in range(100):
+    fig = Figure()
+
+    xvalues, yvalues = get_random_values(1, 10000)
+    fig.plot(xvalues, yvalues, color='red', linewidth=5)
+
+    xvalues, yvalues = get_random_values(1, 10000)
+    fig.plot(xvalues, yvalues, color='blue', linewidth=5)
+
+    fig.save(f'{test_folder}/graph{i}.png')
+
+#-------------------------------------------------------------------------------
